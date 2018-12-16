384eed980c0fe591c64d0071419624fa615961c1
Maximo Bautista <max.bautista97@gmail.com>
2018-12-16 22:35:00 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,15 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
-  
-  
+   if num%3 == 0 && num%5 != 0
+          result = "Fizz"
+          
+          else if(num%5 == 0 && num%3 != 0){
+            console.log("Buzz")
+          }
+          else if(num%5 == 0 && num%3 == 0){
+            console.log("FizzBuzz")
+          }
+          else{console.log(num)
+          }
 end
