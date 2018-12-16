b7dff1649addd225ac107925c1862c493a8d940f
Maximo Bautista <max.bautista97@gmail.com>
2018-12-16 22:36:28 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
-   if num%3 == 0 && num%5 != 0
+   if num % 3 == 0 && num%5 != 0
       result = "Fizz"
-    elseif(num%5 == 0 && num%3 != 0)
+    elseif num % 5 == 0 && num % 3 != 0)
       result = "Buzz"
-    elseif(num%5 == 0 && num%3 == 0)
+    elseif num % 5 == 0 && num % 3 == 0)
       result = "Fizz"
     else
       result = "nil"
