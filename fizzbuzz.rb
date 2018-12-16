08c75f04634fc4478871a9ac032d286b3a0abb8d
Maximo Bautista <max.bautista97@gmail.com>
2018-12-16 22:37:15 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
-  if num % 3 == 0 && num%5 != 0
+  if num % 3 == 0 && num % 5 != 0
     result = "Fizz"
   elseif num % 5 == 0 && num % 3 != 0)
     result = "Buzz"
